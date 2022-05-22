5f2d48960e03b49a7cd915c11b4bc5e611748865
fedechka1245 <fedechka06@gmail.com>
2022-05-23 01:02:03 +0500

    fix money-code-select
    *remove input-value after choosing a money-code

--- src/pages/currentReport/panels/commonInformation/MoneyCodeSelect.vue
+++ src/pages/currentReport/panels/commonInformation/MoneyCodeSelect.vue
@@ -1,13 +1,30 @@
+<template>
+  <q-select
+    ref="select"
+    v-model="store.currentReportStore.moneyCodes"
+    :options="moneyCodes"
+    outlined
+    multiple
+    label="Валюты"
+    use-input
+    use-chips
+    input-debounce="2"
+    @filter="filter"
+    @add="() => select?.updateInputValue('')"
+  />
+</template>
+
 <script setup>
 import * as currencyCodes from "currency-codes";
 import { ref } from "vue";
 import { store } from "@/store/store";
 
-const codes = currencyCodes.codes();
+const select = ref(null);
 
+const codes = currencyCodes.codes();
 const moneyCodes = ref(codes);
 
-const filterFn = (value, update) => {
+const filter = (value, update) => {
   update(() => {
     if (value === "") {
       moneyCodes.value = codes;
@@ -19,27 +36,4 @@ const filterFn = (value, update) => {
     }
   });
 };
-
-const createValue = (val, done) => {
-  if (val.length > 2) {
-    if (!codes.includes(val)) {
-      done(val, "add-unique");
-    }
-  }
-};
 </script>
-
-<template>
-  <q-select
-    v-model="store.currentReportStore.moneyCodes"
-    :options="moneyCodes"
-    outlined
-    multiple
-    label="Валюты"
-    use-input
-    use-chips
-    input-debounce="0"
-    @new-value="createValue"
-    @filter="filterFn"
-  />
-</template>
