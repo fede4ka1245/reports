488655e1bfde10189ed5dffb86156f826e666dc4
fedechka1245 <fedechka06@gmail.com>
2022-05-30 18:13:49 +0500

    delete trash from App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,15 @@
     <modal-root />
   </div>
   <div class="tabbar-margin"></div>
-  {{ data.conversion }}
   <app-tabbar />
 </template>
 
 <script setup>
 import AppTabbar from "./components/appTabbar/AppTabbar";
 import ModalRoot from "@/modalPages/modalRoot/ModalRoot";
-import { onMounted, reactive } from "vue";
+import { onMounted } from "vue";
 import { store } from "./store/store";
 import { getItem, storageKey } from "@/modules/localStorage";
-import { convertToRUB } from "@/modules/conversion/convertToRUB";
-
-const data = reactive({
-  conversion: 0,
-});
-
-convertToRUB(1, "USD", "01", "03", "2022").then(
-  (result) => (data.conversion = result)
-);
 
 onMounted(() => {
   if (getItem(storageKey.currentReport)) {
