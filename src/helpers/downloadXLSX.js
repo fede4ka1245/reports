f2a3560bb5f7f047e50c7f071f556c3044ad3575
fede4ka1245 <bragin.fedor96@gmail.dom>
2022-06-19 21:26:01 +0500

    fix xlsx file loading

--- src/helpers/downloadXLSX.js
+++ src/helpers/downloadXLSX.js
@@ -1,23 +1,25 @@
-import { writeFile } from "capacitor-blob-writer";
-import { Directory } from "@capacitor/filesystem";
+import { Directory, Filesystem } from "@capacitor/filesystem";
+import { Capacitor } from '@capacitor/core';
+import {nanoid} from "nanoid";
 
-export const downloadXLSX = (data, name) => {
-  const blob = new Blob([data], {
-    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-  });
-
-  const a = document.createElement("a");
-  document.body.appendChild(a);
-  a.style = "display: none";
-  const url = window.URL.createObjectURL(blob);
-  a.href = url;
-  a.download = `${name}.xlsx`;
-  a.click();
-  window.URL.revokeObjectURL(url);
-
-  writeFile({
-    path: `Documents/${name}.xlsx`,
-    data: blob,
-    directory: Directory.ExternalStorage,
-  });
+export const downloadXLSX = (fileData, name) => {
+  if (Capacitor.isNativePlatform()) {
+    Filesystem.writeFile({
+      path: `Documents/${name || nanoid()}.xlsx`,
+      data: fileData.toString("base64"),
+      directory: Directory.ExternalStorage,
+    });
+  } else {
+    const blob = new Blob([fileData], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    const url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = `${name || nanoid()}.xlsx`;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
 };
