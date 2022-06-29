d08bfba81f33afb959e62d9e9979691c49650d4c
fedechka1245 <bragin.fedor96@gmail.com>
2022-06-29 16:38:47 +0500

    delete unused file

--- src/helpers/excel/helpers/index.js
+++ src/helpers/excel/helpers/index.js
@@ -1,12 +1,10 @@
 import { createHeaderCell } from "@/helpers/excel/helpers/createHeaderCell";
-import { downloadWorkbook } from "@/helpers/excel/helpers/downloadWorkbook";
 import { setColumnsWidth } from "@/helpers/excel/helpers/setColumnsWidth";
 import { styleSheet } from "@/helpers/excel/helpers/styleSheet";
 import { fillEmptyCells } from "@/helpers/excel/helpers/fillEmptyCells";
 
 export {
   createHeaderCell,
-  downloadWorkbook,
   setColumnsWidth,
   styleSheet,
   fillEmptyCells,
