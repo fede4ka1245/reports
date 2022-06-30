b52c2cfdac8a36b096f18f60f8ea35a43de38080
fedechka1245 <bragin.fedor96@gmail.com>
2022-06-30 14:21:26 +0500

    format sheets

--- src/helpers/excel/helpers/styleSheet.js
+++ src/helpers/excel/helpers/styleSheet.js
@@ -4,4 +4,22 @@ import { fillEmptyCells } from "@/helpers/excel/helpers/fillEmptyCells";
 export function styleSheet(sheet) {
   setColumnsWidth(sheet, 36);
   fillEmptyCells(sheet);
+
+  const { rowCount, columnCount } = sheet;
+
+  for (let rowIndex = 1; rowIndex <= rowCount; rowIndex++) {
+    for (let columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+      sheet.getCell(rowIndex, columnIndex).font = {
+        ...sheet.getCell(rowIndex, columnIndex).font,
+        name: 'Arial',
+        size: 10,
+      };
+
+
+      sheet.getCell(rowIndex, columnIndex).alignment = {
+        horizontal: "center",
+        vertical: "middle",
+      };
+    }
+  }
 }
