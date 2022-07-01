2b7c3a8bb4715318bc1755e2c542de61e61a032e
fedechka1245 <bragin.fedor96@gmail.com>
2022-07-01 23:32:38 +0500

    fix payments in sheet

--- src/helpers/excel/sheetDataSetters/setPayments.js
+++ src/helpers/excel/sheetDataSetters/setPayments.js
@@ -8,18 +8,18 @@ export function setPayments(sheet, outgoingPayments) {
     createHeaderCell(sheet.getCell(rowIndex, 1), label);
     rowIndex += 1;
 
-    payment.type !== "office" && createHeaderCell(sheet.getCell(rowIndex, 2), "[id] ФИО");
-    payment.type !== "member" && createHeaderCell(sheet.getCell(rowIndex, 2), "участник");
+    payment.type !== "office" && payment.type !== "members" && createHeaderCell(sheet.getCell(rowIndex, 2), "[id] ФИО");
+    payment.type === "members" && createHeaderCell(sheet.getCell(rowIndex, 2), "участник");
     createHeaderCell(sheet.getCell(rowIndex, 3), "Сумма");
     createHeaderCell(sheet.getCell(rowIndex, 4), "Дата");
     createHeaderCell(sheet.getCell(rowIndex, 5), "Комментарий");
     rowIndex += 1;
 
     for (let i = 0; i < payments?.length; i++) {
-      payment.type !== "office" && (
+      payment.type !== "office" && payment.type !== "members" && (
         sheet.getCell(rowIndex, 2).value = `[${payments[i].instructorId}] ${payments[i].name}` || "-"
       );
-      payment.type !== "member" && (sheet.getCell(rowIndex, 2).value = payments[i].name);
+      payment.type === "members" && (sheet.getCell(rowIndex, 2).value = payments[i].name);
       sheet.getCell(rowIndex, 3).value = `${payments[i].sum} ${payments[i].moneyCode}`;
       sheet.getCell(rowIndex, 4).value = payments[i].date || "-";
       sheet.getCell(rowIndex, 5).value = payments[i].comment || "-";
