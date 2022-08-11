de7e77a5da174a8c4a2d60c13f10d92b88df2060
fedechka1245 <bragin.fedor96@gmail.com>
2022-08-11 22:03:39 +0300

    fix bug with current reports

--- src/helpers/reports/getEmptyReport.js
+++ src/helpers/reports/getEmptyReport.js
@@ -1,9 +1,60 @@
-import { currentReport } from "./currentReport";
 import { nanoid } from "nanoid";
 
 export const getEmptyReport = () => {
-  const report = currentReport;
-  report.key = nanoid();
-
-  return report;
+  return {
+    key: nanoid(),
+    name: "",
+    dates: "",
+    hikeId: "",
+    moneySums: {},
+    moneyCodes: ["RUB"],
+    members: "",
+    inactiveMembers: "",
+    expenses: [],
+    conversions: [],
+    incomingPayments: [
+      {
+        type: "members",
+        label: "Оплаты участников с исключениями",
+        payments: [],
+      },
+      {
+        type: "instructors",
+        label: "Получено от инструкторов",
+        payments: [],
+      },
+      {
+        type: "instructors",
+        label: "Получено от кураторов",
+        payments: [],
+      },
+      {
+        type: "office",
+        label: "Получено из офиса",
+        payments: [],
+      },
+    ],
+    outgoingPayments: [
+      {
+        type: "instructors",
+        label: "Доходы инструктора",
+        payments: [],
+      },
+      {
+        type: "instructors",
+        label: "Передано инструкторам",
+        payments: [],
+      },
+      {
+        type: "instructors",
+        label: "Передано кураторам",
+        payments: [],
+      },
+      {
+        type: "office",
+        label: "Передано в офис",
+        payments: [],
+      },
+    ],
+  };
 };
