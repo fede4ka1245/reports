40c46a8bcd9440f45998901bb84f39a0dfcc9052
fede4ka1245 <bragin.fedor96@gmail.com>
2023-02-17 15:40:28 +0500

    fix routes list reports editing

--- src/pages/allReports/panels/routesList/components/routesListTable/cellEditReport/CellEditReport.vue
+++ src/pages/allReports/panels/routesList/components/routesListTable/cellEditReport/CellEditReport.vue
@@ -1,17 +1,22 @@
 <template>
   <q-td>
     <q-btn round flat color="grey" icon="more_horiz">
-      <q-menu style="width: 150px">
-        <q-list v-if="store.currentReport?.key !== row?.key">
+      <q-menu style="width: 150px" v-if="store.currentReport?.key !== row?.key">
+        <q-list>
           <q-item
-            v-close-popup
             class="item"
             clickable
-            @click="props.promoteToCurrent(props.row)"
+            @click="onPromoteClick"
+            v-close-popup
           >
             Сделать текущим
           </q-item>
+          <q-item v-close-popup clickable @click="props.remove(props.row)">
+            Удалить
+          </q-item>
         </q-list>
+      </q-menu>
+      <q-menu style="width: 150px" v-else>
         <q-list>
           <q-item v-close-popup clickable @click="props.remove(props.row)">
             Удалить
@@ -39,4 +44,10 @@ const props = defineProps({
     default: undefined,
   },
 });
+
+const onPromoteClick = (event) => {
+  event.stopPropagation();
+
+  props.promoteToCurrent(props.row);
+}
 </script>
